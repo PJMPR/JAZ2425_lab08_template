0de5590e16c4d9ec038ec4249ecaeba05a9e808e
aurbanow <aurbanow@pjwstk.edu.pl>
2024-11-28 15:11:26 +0100

    lab 09

--- Westeros/westeros-movies-client/src/main/java/com/westeros/moviesclient/contract/ActorDto.java
+++ Westeros/westeros-movies-client/src/main/java/com/westeros/moviesclient/contract/ActorDto.java
@@ -1,12 +1,14 @@
 package com.westeros.moviesclient.contract;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.time.LocalDate;
 
 public record ActorDto(long id,
                        String name,
                        String character,
-                       String profilePath,
-                       String biography,
+                       @JsonProperty("profile_path") String profilePath,
+                        String biography,
                        LocalDate birthday,
                        LocalDate deathday,
                        double popularity) {}
